bc210bbab2c560f0588b228e4a3f08e2acdd1a59
huanghungchuan <34148072+huanghungchuan@users.noreply.github.com>
2017-12-16 08:49:11 +0800

    Roman to Integer done

---
+++ src/RomanToInteger.java
@@ -0,0 +1,60 @@
+
+public class RomanToInteger {
+	public int romanToInt(String s) {
+		int ans = 0;
+		LOOP: for (int i = 0; i < s.length(); i++) {
+			if (i < s.length() - 1) {
+				switch (s.substring(i, i + 2)) {
+				case "IV":
+					i++;
+					ans += 4;
+					continue LOOP;
+				case "IX":
+					i++;
+					ans += 9;
+					continue LOOP;
+				case "XL":
+					i++;
+					ans += 40;
+					continue LOOP;
+				case "XC":
+					i++;
+					ans += 90;
+					continue LOOP;
+				case "CD":
+					i++;
+					ans += 400;
+					continue LOOP;
+				case "CM":
+					i++;
+					ans += 900;
+					continue LOOP;
+				}
+			}
+			switch(s.charAt(i)) {
+			case'I':
+				ans += 1;
+				break;
+			case'V':
+				ans += 5;
+				break;
+			case'X':
+				ans += 10;
+				break;
+			case'L':
+				ans += 50;
+				break;
+			case'C':
+				ans += 100;
+				break;
+			case'D':
+				ans += 500;
+				break;
+			case'M':
+				ans += 1000;
+				break;
+			}
+		}
+		return ans;
+	}
+}
