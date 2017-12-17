72161c3cfcf56328a53de31b47073a9e9d9797fe
huanghungchuan <34148072+huanghungchuan@users.noreply.github.com>
2017-12-17 09:16:46 +0800

    #27 Remove Element 8ms, beats 88.14%

--- src/main.java
+++ src/main.java
@@ -1,8 +1,7 @@
 
 public class main {
 	public static void main(String[] args) {
-		/*
-		 * TwoSum
+		/* TwoSum
 		 * 
 		 * TwoSum ts = new TwoSum(); int nums[] = { 1, 2, 3, 4 }; int answer[] =
 		 * ts.twoSum(nums, 4); System.out.println("" + answer[0] + answer[1]);
