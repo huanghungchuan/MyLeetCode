39c104acce99c636777c3426a2a6036fcd97142c
huanghungchuan <34148072+huanghungchuan@users.noreply.github.com>
2017-12-18 09:13:22 +0800

    #35 Search Insert Position Second answer using binary search, same runtime

--- src/SearchInsertPosition.java
+++ src/SearchInsertPosition.java
@@ -1,10 +1,26 @@
 
 public class SearchInsertPosition {
 	public int searchInsert(int[] nums, int target) {
+		/*Version 1, 6ms
 		for (int i = 0; i < nums.length; i++) {
 			if (target <= nums[i])
 				return i;
 		}
 		return nums.length;
+		*/
+		
+		//Version 2, using binary search, same runtime as Version 1
+		int lo = 0;
+		int hi = nums.length-1;
+		while(lo <= hi) {
+			int mid = lo + (hi - lo)/2;
+			if(target < nums[mid])
+				hi = mid-1;
+			else if(target > nums[mid])
+				lo = mid+1;
+			else 
+				return mid;
+		}
+		return lo;
 	}
 }
