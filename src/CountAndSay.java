d9c80d6c0c2d45b718b780407e8b0d9d58ff6a25
huanghungchuan <34148072+huanghungchuan@users.noreply.github.com>
2017-12-19 08:09:51 +0800

    #38 Count And Say 4ms, beats 82.65% fix duplication check

--- src/CountAndSay.java
+++ src/CountAndSay.java
@@ -1,25 +1,22 @@
 
 public class CountAndSay {
 	public String countAndSay(int n) {
-		if(n == 1)
+		if (n == 1)
 			return "1";
-		else {
-			String s = countAndSay(n-1);
-			int count = 1;
-			StringBuilder sb = new StringBuilder();
-			char current = s.charAt(0);
-			for(int i = 1; i<s.length();i++) {
-				if(current != s.charAt(i)) {
-					sb.append(count).append(current);
-					count = 1;
-					current = s.charAt(i);
-				}
-				else {
-					count++;
-				}
+		String s = countAndSay(n - 1);
+		int count = 1;
+		StringBuilder sb = new StringBuilder();
+		char current = s.charAt(0);
+		for (int i = 1; i < s.length(); i++) {
+			if (current != s.charAt(i)) {
+				sb.append(count).append(current);
+				count = 1;
+				current = s.charAt(i);
+			} else {
+				count++;
 			}
-			sb.append(count).append(current);
-			return sb.toString();
 		}
+		sb.append(count).append(current);
+		return sb.toString();
 	}
 }
