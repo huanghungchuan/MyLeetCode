d1f86611e50b927115f16c8d1be3f1d488b28dae
huanghungchuan <34148072+huanghungchuan@users.noreply.github.com>
2017-12-19 08:05:32 +0800

    #38 Count and Say 4ms, beats 82.65%

---
+++ src/CountAndSay.java
@@ -0,0 +1,27 @@
+
+public class CountAndSay {
+	public String countAndSay(int n) {
+		if(n == 1)
+			return "1";
+		else if(n == 2)
+			return "11";
+		else {
+			String s = countAndSay(n-1);
+			int count = 1;
+			StringBuilder sb = new StringBuilder();
+			char current = s.charAt(0);
+			for(int i = 1; i<s.length();i++) {
+				if(current != s.charAt(i)) {
+					sb.append(count).append(current);
+					count = 1;
+					current = s.charAt(i);
+				}
+				else {
+					count++;
+				}
+			}
+			sb.append(count).append(current);
+			return sb.toString();
+		}
+	}
+}
