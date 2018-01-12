80fa17526c12d4a3cea1543d0eb1e504fb6bc519
huanghungchuan <34148072+huanghungchuan@users.noreply.github.com>
2018-01-12 08:01:08 +0800

    #100 Same Tree 6ms

--- src/_100SameTree.java
+++ src/_100SameTree.java
@@ -1,4 +1,18 @@
 
 public class _100SameTree {
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+		//Version 1
+		if (p == null || q == null)
+			return (p == q);
+		if(p.val != q.val)
+			return false;
+		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 
+		//Version 2
+		if (p == null && q == null)
+			return (true);
+		if (p != null && q != null && p.val == q.val)
+			return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+		return false;
+	}
 }
